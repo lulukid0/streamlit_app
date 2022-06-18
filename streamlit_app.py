a95a448efc66cad8fb7c466260409070282341d6
lulukid0 <106764632+lulukid0@users.noreply.github.com>
2022-06-18 16:54:26 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,14 @@
 import streamlit as st
 from streamlit_folium import st_folium
 import folium
-m = folium.Map(location=[-12.110372628922658, -77.03693043439692],zoom_start=15)
-folium.Marker(location=[-12.110372628922658, -77.03693043439692],popup=popuptext).add_to(m)
-folium.Circle(location=[-12.110372628922658, -77.03693043439692],color='purple',fill_color='red',radius=40,weight=-4,fill_opacity=0.5,tooltip='Punto de recolección de datos').add_to(ovalo_miraflores)
-folium.TileLayer('stamenterrain').add_to(m)
 
-st_data=st_folium(m,width=725)
+# center on Liberty Bell, add marker
+m = folium.Map(location=[39.949610, -75.150282], zoom_start=16)
+folium.Marker(
+    [39.949610, -75.150282], 
+    popup="Liberty Bell", 
+    tooltip="Liberty Bell"
+).add_to(m)
 
+# call to render Folium map in Streamlit
+st_data = st_folium(m, width = 725)
